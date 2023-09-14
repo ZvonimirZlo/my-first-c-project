f05932dfde04ece64563bf4cff93975065bf7ab0
ZvonimirZlo <lovrez2@gmail.com>
2023-09-14 21:13:15 +0200

    first commit

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+ 
+  std::cout << "Z Z Z Z Z  L         \n";
+  std::cout << "      Z    L         \n";
+  std::cout << "    Z      L         \n";
+  std::cout << "  Z        L         \n";
+  std::cout << "Z Z Z Z Z  L L L L L \n";
+ 
+ 
+}
